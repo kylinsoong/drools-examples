d5511d3e3d7622e147feedb11bb3f66394723f68
Kylin <songzhiqi_1214@yahoo.com.cn>
2012-07-17 18:07:03 +0800

    add drools api test code

---
+++ sandbox/brms/src/main/java/com/kylin/brms/jbpm/humantask/HumanTaskExecutor.java
@@ -0,0 +1,31 @@
+package com.kylin.brms.jbpm.humantask;
+
+import org.drools.KnowledgeBase;
+import org.drools.builder.KnowledgeBuilder;
+import org.drools.builder.KnowledgeBuilderError;
+import org.drools.builder.KnowledgeBuilderFactory;
+import org.drools.builder.ResourceType;
+import org.drools.io.ResourceFactory;
+import org.drools.runtime.StatefulKnowledgeSession;
+import org.drools.runtime.process.ProcessInstance;
+
+public class HumanTaskExecutor {
+
+	public static void main(String[] args) {
+
+		KnowledgeBuilder kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
+		kbuilder.add(ResourceFactory.newClassPathResource("jbpm/humanTaskTest.bpmn"), ResourceType.BPMN2);
+		if(kbuilder.hasErrors()) {
+			if (kbuilder.getErrors().size() > 0) {
+				for (KnowledgeBuilderError error : kbuilder.getErrors()) {
+					System.err.println(error.toString());
+				}
+			}
+		}
+		KnowledgeBase kbase = kbuilder.newKnowledgeBase();
+		
+		StatefulKnowledgeSession ksession = kbase.newStatefulKnowledgeSession();
+		ProcessInstance processInstance = ksession.startProcess("com.kylin.brms.jbpm.humantask");
+	}
+
+}
