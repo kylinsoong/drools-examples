28f00c14d1bd13b7500df2c08abf754330fc80ad
Kylin <songzhiqi_1214@yahoo.com.cn>
2013-03-08 17:33:48 +0800

    add drools test code

---
+++ drools/helloworld/src/main/java/com/sample/DecisionTableTest.java
@@ -0,0 +1,84 @@
+package com.sample;
+
+import org.drools.KnowledgeBase;
+import org.drools.KnowledgeBaseFactory;
+import org.drools.builder.DecisionTableConfiguration;
+import org.drools.builder.DecisionTableInputType;
+import org.drools.builder.KnowledgeBuilder;
+import org.drools.builder.KnowledgeBuilderError;
+import org.drools.builder.KnowledgeBuilderErrors;
+import org.drools.builder.KnowledgeBuilderFactory;
+import org.drools.builder.ResourceType;
+import org.drools.io.ResourceFactory;
+import org.drools.logger.KnowledgeRuntimeLogger;
+import org.drools.logger.KnowledgeRuntimeLoggerFactory;
+import org.drools.runtime.StatefulKnowledgeSession;
+
+/**
+ * This is a sample class to launch a decision table.
+ */
+public class DecisionTableTest {
+
+    public static final void main(String[] args) {
+        try {
+            // load up the knowledge base
+            KnowledgeBase kbase = readKnowledgeBase();
+            StatefulKnowledgeSession ksession = kbase.newStatefulKnowledgeSession();
+            KnowledgeRuntimeLogger logger = KnowledgeRuntimeLoggerFactory.newFileLogger(ksession, "test");
+            // go !
+            Message message = new Message();
+            message.setMessage("Hello World");
+            message.setStatus(Message.HELLO);
+            ksession.insert(message);
+            ksession.fireAllRules();
+            logger.close();
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
+
+    private static KnowledgeBase readKnowledgeBase() throws Exception {
+        KnowledgeBuilder kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
+        DecisionTableConfiguration config = KnowledgeBuilderFactory.newDecisionTableConfiguration();
+        config.setInputType(DecisionTableInputType.XLS);
+        kbuilder.add(ResourceFactory.newClassPathResource("Sample.xls"), ResourceType.DTABLE, config);
+        KnowledgeBuilderErrors errors = kbuilder.getErrors();
+        if (errors.size() > 0) {
+            for (KnowledgeBuilderError error: errors) {
+                System.err.println(error);
+            }
+            throw new IllegalArgumentException("Could not parse knowledge.");
+        }
+        KnowledgeBase kbase = KnowledgeBaseFactory.newKnowledgeBase();
+        kbase.addKnowledgePackages(kbuilder.getKnowledgePackages());
+        return kbase;
+    }
+
+    public static class Message {
+
+        public static final int HELLO = 0;
+        public static final int GOODBYE = 1;
+
+        private String message;
+
+        private int status;
+
+        public String getMessage() {
+            return this.message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+
+        public int getStatus() {
+            return this.status;
+        }
+
+        public void setStatus(int status) {
+            this.status = status;
+        }
+
+    }
+
+}
---
+++ drools/helloworld/src/main/java/com/sample/Test.java
@@ -0,0 +1,10 @@
+package com.sample;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		
+	}
+
+}
