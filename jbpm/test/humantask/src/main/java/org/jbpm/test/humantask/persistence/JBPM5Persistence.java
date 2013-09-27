32252b8d4e077dca5c0bb1f169d533f3a48cbc97
Kylin <kylinsoong.1214@gmail.com>
2013-09-27 15:43:08 +0800

    update jbpm persist code

--- jbpm/test/humantask/src/main/java/org/jbpm/test/humantask/persistence/JBPM5Persistence.java
+++ jbpm/test/humantask/src/main/java/org/jbpm/test/humantask/persistence/JBPM5Persistence.java
@@ -7,6 +7,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 import org.drools.KnowledgeBase;
+import org.drools.SystemEventListenerFactory;
 import org.drools.builder.KnowledgeBuilder;
 import org.drools.builder.KnowledgeBuilderFactory;
 import org.drools.builder.ResourceType;
@@ -16,9 +17,12 @@ import org.drools.persistence.jpa.JPAKnowledgeService;
 import org.drools.runtime.Environment;
 import org.drools.runtime.EnvironmentName;
 import org.drools.runtime.StatefulKnowledgeSession;
+import org.drools.runtime.process.ProcessInstance;
+import org.jbpm.process.workitem.wsht.SyncWSHumanTaskHandler;
+import org.jbpm.task.TaskService;
 import org.jbpm.task.service.DefaultUserGroupCallbackImpl;
 import org.jbpm.task.service.UserGroupCallbackManager;
-import org.jbpm.test.JBPMHelper;
+import org.jbpm.task.service.local.LocalTaskService;
 
 import bitronix.tm.TransactionManagerServices;
 import bitronix.tm.resource.jdbc.PoolingDataSource;
@@ -39,11 +43,31 @@ public class JBPM5Persistence {
         env.set(EnvironmentName.ENTITY_MANAGER_FACTORY, emf);
         env.set(EnvironmentName.TRANSACTION_MANAGER, TransactionManagerServices.getTransactionManager());
         
-        StatefulKnowledgeSession ksession = JPAKnowledgeService.newStatefulKnowledgeSession(kbase, null, env);
+        // JPAKnowledgeService create new StatefulKnowledgeSession
+//        StatefulKnowledgeSession ksession = JPAKnowledgeService.newStatefulKnowledgeSession(kbase, null, env);
+//        System.out.println(ksession.getId());
         
+        // JPAKnowledgeService load exist StatefulKnowledgeSession (use "select id from SessionInfo;" get exist session id list in db)
+        StatefulKnowledgeSession ksession = JPAKnowledgeService.loadStatefulKnowledgeSession(3, kbase, null, env);
         System.out.println(ksession.getId());
         
+        TaskService taskService = initTaskService(ksession);
+        
+        ProcessInstance pInstance = ksession.startProcess("com.sample.bpmn.hello");
+		System.out.println("Start Process...: " + pInstance.getId());
+        
         System.exit(0);
+	}
+	
+	private static TaskService initTaskService(StatefulKnowledgeSession ksession) {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("org.jbpm.persistence.jpa");
+		org.jbpm.task.service.TaskService taskService = new org.jbpm.task.service.TaskService(emf, SystemEventListenerFactory.getSystemEventListener());
+		LocalTaskService localTaskService = new LocalTaskService(taskService);
+		SyncWSHumanTaskHandler humanTaskHandler = new SyncWSHumanTaskHandler(localTaskService, ksession);
+		humanTaskHandler.setLocal(true);
+		humanTaskHandler.connect();
+		ksession.getWorkItemManager().registerWorkItemHandler("Human Task", humanTaskHandler);
+		return localTaskService;
 	}
 	
 	private static void setupDataSource() {
