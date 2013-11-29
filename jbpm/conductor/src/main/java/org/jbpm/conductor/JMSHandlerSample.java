07b9772a6f2deee94995dd6fef6701c8a1cc8996
Kylin <kylinsoong.1214@gmail.com>
2013-11-29 17:56:54 +0800

    add jmx handler code

--- jbpm/conductor/src/main/java/org/jbpm/conductor/JMSHandlerSample.java
+++ jbpm/conductor/src/main/java/org/jbpm/conductor/JMSHandlerSample.java
@@ -21,7 +21,6 @@ import org.jbpm.task.Status;
 import org.jbpm.task.query.TaskSummary;
 import org.jbpm.task.service.AsyncTaskServiceWrapper;
 import org.jbpm.task.service.TaskClient;
-import org.jbpm.task.service.hornetq.CommandBasedHornetQWSHumanTaskHandler;
 import org.jbpm.task.service.jms.JMSTaskClientConnector;
 import org.jbpm.task.service.jms.JMSTaskClientHandler;
 
@@ -34,58 +33,35 @@ public class JMSHandlerSample {
 		KnowledgeBase kbase = readKnowledgeBase();
         StatefulKnowledgeSession ksession = kbase.newStatefulKnowledgeSession();
         
-        TaskClient clientForHumanTaskHandler = getTaskClientInstance();
-        boolean success = clientForHumanTaskHandler.connect("localhost", 1099);
+        TaskClient client = getTaskClientInstance();
+        boolean success = client.connect();
         System.out.println("Connecting to human task service, success: " + success);
         
-        CommandBasedHornetQWSHumanTaskHandler handler = new CommandBasedHornetQWSHumanTaskHandler(ksession);
-        handler.setClient(clientForHumanTaskHandler);
-        handler.connect();
+//        Thread.sleep(3000);
+        
+        AsyncWSHumanTaskHandler handler = new AsyncWSHumanTaskHandler(client, ksession);
         
         ksession.getWorkItemManager().registerWorkItemHandler("Human Task", handler);
         
-      //start a new process instance
         ProcessInstance pi = ksession.startProcess("com.sample.bpmn.hello");
         System.out.println("Process started ... : pid = " + pi.getId());
         
-		// Wait to make sure that Task is committed
-        Thread.sleep(3000);
-        
-        TaskClient clientForHumanTaskClient = getTaskClientInstance();
-        clientForHumanTaskClient.connect("localhost", 1099);
-        AsyncTaskServiceWrapper taskService = new AsyncTaskServiceWrapper(clientForHumanTaskClient);
+        AsyncTaskServiceWrapper taskService = new AsyncTaskServiceWrapper(client);
         List<TaskSummary> list = taskService.getTasksAssignedAsPotentialOwner("john", "en-UK");
-        for (TaskSummary task : list) {
-            System.out.println("task.getId() = " + task.getId());
-            System.out.println("task.getProcessInstanceId() = " + task.getProcessInstanceId());
-            if (task.getStatus() == Status.Reserved) {
-                System.out.println("John is starting and completing task " + task.getName());
-                taskService.start(task.getId(), "john");
-                taskService.complete(task.getId(), "john", null);
-            }
-            if (task.getStatus() == Status.InProgress) {
-                System.out.println("John is completing task " + task.getName());
-                taskService.complete(task.getId(), "john", null);
-            }
+        for (TaskSummary task : list){
+        	System.out.println("task.getId() = " + task.getId());
+			if (task.getStatus() == Status.Reserved) {
+				System.out.println("John is starting and completing task, id = " + task.getId() + ", name = " + task.getName());
+				taskService.start(task.getId(), "john");
+				taskService.complete(task.getId(), "john", null);
+			}
         }
         
-        Thread.sleep(3000);
+//        Thread.sleep(3000);
         
-		taskService.disconnect();
+//        taskService.disconnect();
         
-        clientForHumanTaskHandler.disconnect();
-        
-        System.out.println("taskService is disconnected");
-        
-        System.exit(0);
-        
-//        AsyncWSHumanTaskHandler handler = new AsyncWSHumanTaskHandler(client, ksession);
-//        handler.setConnection("localhost", 1099);
-//        
-//        ksession.getWorkItemManager().registerWorkItemHandler("Human Task", handler);
-//        
-//        ProcessInstance pi = ksession.startProcess("com.sample.bpmn.hello");
-//        System.out.println("Process started ... : pid = " + pi.getId());
+//        client.disconnect();
 		
 	}
 	
