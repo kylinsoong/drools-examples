70b1fed4066589007d27d40a8b35aa364bb6610f
Kylin <kylinsoong.1214@gmail.com>
2014-07-18 11:53:27 +0800

    add case reproduce

--- drools-6-examples/kie-build/src/main/java/org/drools/SupportCase01144843.java
+++ drools-6-examples/kie-build/src/main/java/org/drools/SupportCase01144843.java
@@ -1,8 +1,16 @@
 package org.drools;
 
+import java.io.File;
+
 import org.kie.api.KieBase;
 import org.kie.api.KieServices;
+import org.kie.api.builder.KieBuilder;
 import org.kie.api.runtime.KieContainer;
+import org.kie.internal.builder.JaxbConfiguration;
+import org.kie.internal.builder.KnowledgeBuilderFactory;
+
+import com.sun.tools.xjc.Language;
+import com.sun.tools.xjc.Options;
 
 public class SupportCase01144843 {
 
@@ -10,6 +18,13 @@ public class SupportCase01144843 {
 		KieServices ks = KieServices.Factory.get();
 		KieContainer kContainer = ks.getKieClasspathContainer();
 		KieBase kBase = kContainer.getKieBase("kbase");
+		
+		Options xjcOpts = new Options();
+		xjcOpts.setSchemaLanguage(Language.XMLSCHEMA);
+		JaxbConfiguration jaxbConfiguration = KnowledgeBuilderFactory.newJaxbConfiguration( xjcOpts, "xsd" );
+		
+		KieBuilder kbuilder = ks.newKieBuilder(new File(""));
+
 	}
 
 }
